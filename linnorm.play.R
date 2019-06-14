d0b3ff58c7ba900632d5da9d94943dd95ca0bf2f
Sharon <xueyuanjiang2013@gmail.com>
2019-06-14 13:49:58 -0400

    Update linnorm.play.R

--- linnorm.play.R
+++ linnorm.play.R
@@ -34,16 +34,49 @@ ad_wt_table = data.frame(admegatable, wtmegatable)
 # save a copy of this file
 saveRDS(ad_wt_table, file = "./pbmc3k_final.rds")
 
-# get the processed data (read count)
-countfile = list.files(path = "~/Documents/2019_summer_eisai/raw_data/GSE98969_RAW", pattern = "GSM" )
+ad_wt_table = readRDS("dam.ad_wt.umi.merged.rds")
 
-testdf = read.csv(list.files(pattern = as.character(adfiles[1])), sep="\t", header=TRUE)
 
-table(grepl("^ERCC-", rownames(testdf)))
-table(rownames(testdf) %in% 
-        c("ENSG00000198899", "ENSG00000198727", "ENSG00000198888",
-          "ENSG00000198886", "ENSG00000212907", "ENSG00000198786",
-          "ENSG00000198695", "ENSG00000198712", "ENSG00000198804",
-          "ENSG00000198763", "ENSG00000228253", "ENSG00000198938",
-          "ENSG00000198840"))
-# save a copy of the mega matrix
+# Seurat version
+
+ad_wt_seurat = CreateSeuratObject(counts = ad_wt_table, project = "dam", min.cells = 1, min.features = 200)
+ad_wt_seurat[["percent.ercc"]] = PercentageFeatureSet(ad_wt_seurat, pattern ="^ERCC-" )
+
+VlnPlot(ad_wt_seurat, features = c("nFeature_RNA", "nCount_RNA", "percent.ercc"), ncol = 3)
+
+cor_plot1 = FeatureScatter(ad_wt_seurat, feature1 = "nCount_RNA", feature2 = "percent.ercc")
+cor_plot2 = FeatureScatter(ad_wt_seurat, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+CombinePlots(plots = list(cor_plot1, cor_plot2))
+
+ad_wt_seurat_filter = subset(ad_wt_seurat, subset = nFeature_RNA > 200 & nFeature_RNA < 2500)
+
+ad_wt_seurat_filter = NormalizeData(ad_wt_seurat_filter)
+
+# select the 220 most variable features 
+ad_wt_seurat_filter = FindVariableFeatures(ad_wt_seurat_filter, selection.method = "vst", nfeatures = 220)
+
+seurat_top10 = head(VariableFeatures(ad_wt_seurat_filter), 10)
+
+var_plot1 = VariableFeaturePlot(ad_wt_seurat_filter)
+var_plot2 = LabelPoints(plot = var_plot1, points = seurat_top10, repel = T)
+CombinePlots(plots = list(var_plot1, var_plot2))
+
+# scale the data, perform dimension reduction
+all.genes = rownames(ad_wt_seurat_filter)
+ad_wt_seurat_filter = ScaleData(ad_wt_seurat_filter, features = all.genes)
+
+ad_wt_seurat_filter = RunPCA(ad_wt_seurat_filter, features = VariableFeatures(object = ad_wt_seurat_filter))
+
+DimPlot(ad_wt_seurat_filter, reduction = "pca")
+
+DimHeatmap(ad_wt_seurat_filter, dims = 1, cells = 500, balanced = T)
+
+
+ad_wt_seurat_filter = FindNeighbors(ad_wt_seurat_filter, dims = 1:10)
+ad_wt_seurat_filter = FindClusters(ad_wt_seurat_filter, resolution = 0.5)
+ad_wt_seurat_filter = RunUMAP(ad_wt_seurat_filter, dims = 1:10)
+DimPlot(ad_wt_seurat_filter, reduction = "umap")
+
+
+
+
